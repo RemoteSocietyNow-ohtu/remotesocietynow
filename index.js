47137ca6249f9d14361b721db1374cf6b6af4a24
universami <sami.toiskallio@protonmail.com>
2020-05-11 16:20:01 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.use(express.static('build'))
+
+const cors = require('cors')
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Backend is running!</h1>')
+})
+
+const port = process.env.PORT || 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
