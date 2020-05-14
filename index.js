48760a444f1d77c9cb2dbe4254b7e89dcd366f5f
Sami <samialeksi.tuomela@gmail.com>
2020-05-14 13:57:10 +0300

    lisätty alustava lista kysymyksistä

--- index.js
+++ index.js
@@ -3,7 +3,46 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-
+const questions = [
+  {
+    name: 'Kuinka pitkä työmatkaasi on kilometreinä? Ilmoita matka yhteen suuntaan.',
+    type: 'field'
+  },
+  {
+    name: 'Millä kulkuvälineellä useimmiten kuljet töihin?',
+    type: 'multipleChoice',
+    options: [
+      'Jalan/Pyörällä',
+      'Linja-auto',
+      'Juna',
+      'Auto',
+      'Moottoripyörä'
+    ]
+  },
+  {
+    name: 'Kuinka monena päivänä viikossa kuljet tällä kulkuvälineellä töihin?',
+    type: 'field"
+  },
+  {
+    name: 'Kuljetko jollakin muulla kulkuvälineellä töihin, ja jos kyllä, millä?',
+    type: 'multipleChoice',
+    options: [
+      'Jalan/Pyörällä',
+      'Linja-auto',
+      'Juna',
+      'Auto',
+      'Moottoripyörä'
+    ]
+  },
+  {
+    name: 'Kuinka monena päivänä viikossa keskimäärin teet etätöitä?',
+    type: 'field"
+  },
+  {
+    name: 'Miten suhtaudut etätyöskentelyyn?',
+    type: 'slider'
+  }
+]
 
 app.use(express.static(__dirname + '/frontend/build/'))
 
