452603134af1c8f4ac5a37d4d7a30b02a7a5ac11
Sami <samialeksi.tuomela@gmail.com>
2020-05-19 14:12:24 +0300

    added questionRouter, handle's providing questions

--- frontend/src/services/questionService.js
+++ frontend/src/services/questionService.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 const baseurl = process.env.REACT_APP_BACKEND_URL ? process.env.REACT_APP_BACKEND_URL : ''
 
 const getQuestions = () => {
-  const request = axios.get(`${baseurl}/api/questions/`)
+  const request = axios.get(`${baseurl}/api/questions/people/`)
   return request.then((res) => res.data)
 }
 
 const sendAnswers = ( values ) => {
-  const request = axios.post(`${baseurl}/api/calculate/person`, values)
+  const request = axios.post(`${baseurl}/api/calculate/person/`, values)
   return request.then((res) => res.data)
 }
 
