90ea68dbbdb6eb5e9ed676966517b5a48a1aefa4
Sami <samialeksi.tuomela@gmail.com>
2020-05-19 12:15:42 +0300

    added controller for calculation

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 app.use(express.json())
+const calculatePerson = require('./controllers/calculateperson')
+
 
 const questions = [
   {
@@ -98,38 +100,8 @@ app.get('/api/questions/:id', (req, res) => {
   res.json(question)
 })
 
-app.post('/api/calculate/', (req,res) => {
-  console.log(req.body)
+app.use('/api/calculate/', calculatePerson)
 
-  const distance = +req.body.dailyCommuteKm
-  const days = +req.body.noOfDaysOfUsage
-  const remoteDays = +req.body.numberOfRemoteworkDays
-  const vehicle = req.body.typicalVehicle
-  const secondVehicle = req.body.secondVehicle
-  const daysSecond = +req.body.noOfDaysOfUsageSecond
-
-  const coefficients = {}
-  coefficients['car'] = 155
-  coefficients['train'] = 10
-  coefficients['bus'] = 53
-  coefficients['walking'] = 0
-  coefficients['motorcycle'] = 91
-  
-  const coefficientOfUser = (days*coefficients[vehicle]+daysSecond*coefficients[secondVehicle])/(days+daysSecond)
-  const shareOfWorkDoneAtOffice = Math.max(1-(remoteDays/(days+daysSecond)),0)
-  
-  let co2 = coefficientOfUser*distance*2*221
-  const co2remote = coefficientOfUser*distance*2*shareOfWorkDoneAtOffice*221
-  
-  const co2reduce = Math.round(((co2-co2remote)/1000 + Number.EPSILON) * 100) / 100
-  co2 =  Math.round((co2/1000 + Number.EPSILON) * 100) / 100
-
-  const result ={
-    co2: co2,
-    co2reduce: co2reduce
-  }
-  res.json(result)
-})
 
 const port = process.env.PORT || 3001
 app.listen(port)
