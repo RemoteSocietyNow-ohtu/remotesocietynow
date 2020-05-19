d9599f5d970c81a85a2245e9c8af663788e992e8
Tambourin <olavi.hartonen@gmail.com>
2020-05-19 13:23:48 +0300

    Added language and language context

---
+++ frontend/src/resources/language.js
@@ -0,0 +1,20 @@
+export const english = {
+  name: 'English',
+  headers: {
+    mainHeader: 'Remote Society -Now!',
+    companies: 'companies',
+    people: 'people',
+    calculator: 'calculator'
+  },
+  buttons: {
+    calculate: 'Calculate'
+  },
+  navigation: {
+    forPeople: 'For people',
+    forCompany: 'For company'
+  },
+  results: {
+    commuteCO2result: 'Annual CO2 pollution:',
+    commuteCO2savings: 'Annual CO2 savings:' 
+  }
+}
