2daba30b99a962b99838663fa24d3022da15bc66
Sami <samialeksi.tuomela@gmail.com>
2020-05-20 13:11:45 +0300

    changed results css

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -5,11 +5,14 @@ const Results = ({ results }) => {
   const language = useContext(LanguageContext)
 
   return (
-    <div>     
-      <p>{language.results.commuteCO2result}:</p>
-      <p>{results.co2 ? results.co2 : '-'} {language.units.kg}</p> 
-      <p>{language.results.commuteCO2savings}:</p>
-      <p>{results.co2reduce ? results.co2reduce : '-'} {language.units.kg}</p>
+    <div className='Container'>
+      <div className='Results'>
+        <p>{language.results.commuteCO2result}</p>
+        <p>{results.co2 ? results.co2 : '-'} {language.units.kg}</p>
+        <div className='Line-separator-full' /> 
+        <p>{language.results.commuteCO2savings}</p>
+        <p>{results.co2reduce ? results.co2reduce : '-'} {language.units.kg}</p>
+      </div>
     </div>
   )
 }
