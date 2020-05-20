765195ec570f5a4c38a6b63fb5cad99c6bc4c9f5
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-05-20 14:08:35 +0300

    add endpoint for fetching company questions

--- controllers/questionRouter.js
+++ controllers/questionRouter.js
@@ -1,9 +1,14 @@
 const questionRouter = require('express').Router()
 
 const questionsPeople = require('../services/questions/questionsPeople')
+const questionsCompanies = require('../services/questions/questionsCompanies')
 
 questionRouter.get('/people/', (req, res) => {
   res.json(questionsPeople)
+})
+
+questionRouter.get('/companies/', (req,res) => {
+  res.json(questionsCompanies)
 })
 
 questionRouter.get('/people/:id', (req, res) => {
