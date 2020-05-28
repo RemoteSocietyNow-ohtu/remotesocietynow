42d584fc833cd5303a145a64e31ca6dd1e593ecb
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-05-28 16:31:30 +0300

    fix test

--- server/tests/services/remoteWorkCalculator.test.js
+++ server/tests/services/remoteWorkCalculator.test.js
@@ -3,6 +3,6 @@ const remoteWorkCalculator = require('../../services/calculations/remoteWorkCalc
 test('emissions of 12 km of commute, thrice with car, twice with bus, two remotedays', ()=>{
   const result = remoteWorkCalculator.calculateBenefitsForPerson(12,3,2,'car','bus',2)
 
-  expect(result[0].value).toBe(605.72)
+  expect(result[0].value).toBe(363.43)
   expect(result[1].value).toBe(242.29)
 })
