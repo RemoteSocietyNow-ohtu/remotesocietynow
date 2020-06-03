659be25493fdb094b16115a863e1c31dfeb7ee8f
Sami <samialeksi.tuomela@gmail.com>
2020-06-03 14:10:46 +0300

    added checks to only show ticks on day sliders

--- client/components/InputFields/SliderField.js
+++ client/components/InputFields/SliderField.js
@@ -1,49 +1,22 @@
 import React from 'react'
+import TicksSlider from '../SubComponents/TicksSlider'
 
 const SliderField = ({ handleValueChange, handleRelease, value, minValue, maxValue, unit }) => {
-  const width = {
-    width:'14.13%'
-  }
   
   return(
     <div>
       <div className='Calculator-sliderfield-container'>
-      <input
-        type="range" 
-        min={minValue} 
-        max={maxValue}
-        onChange={handleValueChange}  
-        onMouseUp={handleRelease}
-        onTouchEnd={handleRelease}      
-        value={value}
-        className='Calculator-sliderfield'
-      />
-      <table style={{marginLeft: '3%'}}>
-        <td style={width}>
-          <div className='Calculator-sliderfield-tick'></div>
-        </td>
-        <td style={width}>
-          <div className='Calculator-sliderfield-tick'></div>
-        </td>
-        <td style={width}>
-          <div className='Calculator-sliderfield-tick'></div>
-        </td>
-        <td style={width}>
-          <div className='Calculator-sliderfield-tick'></div>
-        </td>
-        <td style={width}>
-          <div className='Calculator-sliderfield-tick'></div>
-        </td>
-        <td style={width}>
-          <div className='Calculator-sliderfield-tick'></div>
-        </td>
-        <td style={width}>
-          <div className='Calculator-sliderfield-tick'></div>
-        </td>
-        <td>
-          <div className='Calculator-sliderfield-tick'></div>
-        </td>
-      </table>
+        <input
+          type="range" 
+          min={minValue} 
+          max={maxValue}
+          onChange={handleValueChange}  
+          onMouseUp={handleRelease}
+          onTouchEnd={handleRelease}      
+          value={value}
+          className='Calculator-sliderfield'
+        />
+        {maxValue === '7' ? <TicksSlider width='14.13%' /> : null}
       </div>
       <p>{value} {unit}</p>
     </div>
