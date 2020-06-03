f8a279852ad11e9293305e0d10acc7d22a7f4424
Tambourin <olavi.hartonen@gmail.com>
2020-06-03 15:28:03 +0300

    Fixed: PrivacyCheckbox is now image

--- client/components/InputFields/PrivacyCheckbox.js
+++ client/components/InputFields/PrivacyCheckbox.js
@@ -1,16 +1,25 @@
 import React, { useContext } from 'react'
 import LanguageContext from '../../Contexts/LanguageContext'
+import checkboxImage from '../../resources/checkbox.png'
+import checkboxImageChecked from '../../resources/checkbox-checked.png'
 
 const PrivacyCheckbox = ({ checked, setChecked, setBody }) => {
   const language = useContext(LanguageContext)
+
   return (
-    <div onClick={setChecked}>
-      <input
+    <div className='CalculatorChoice-checkbox-container' onClick={setChecked}>      
+      <button
         className='CalculatorChoice-checkbox'
         type='checkbox'
         checked={checked}    
         onChange={setChecked}    
-      />
+      >
+        {checked ? 
+          <img className='CalculatorChoice-checkbox-image' src={checkboxImageChecked} /> 
+          : 
+          <img className='CalculatorChoice-checkbox-image' src={checkboxImage} />
+        }
+      </button>
       <label className='CalculatorChoice-checkbox-label' >
         {language.content.privacyPolicy} 
         <span className='CalculatorChoice-privacy-policy-link' onClick={() => setBody('privacy-policy')}> {language.headers.privacyPolicy}.</span>      
--- client/components/SubComponents/Calculator/Stepper.js
+++ client/components/SubComponents/Calculator/Stepper.js
@@ -3,8 +3,7 @@ import React from 'react'
 const Stepper = ({ questions, currentQuestion, setCurrentQuestion }) => {
   return (
     <div className='Calculator-stepper-container'>
-      <ul className='Calculator-stepper'>
-        
+      <ul className='Calculator-stepper'>        
         {questions.map(q => {
           if(q.number === currentQuestion) {
             return <li className='active' onClick={() => setCurrentQuestion(q.number)}></li>
