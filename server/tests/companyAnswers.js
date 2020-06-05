1c964d438e137ca0f6b4a55053dd2a863c2d23d5
Sami <samialeksi.tuomela@gmail.com>
2020-06-05 13:52:43 +0300

    changed companyAnswers in test to answer to current questions

--- server/tests/companyAnswers.js
+++ server/tests/companyAnswers.js
@@ -1,14 +1,36 @@
+import companyQuestions from '../services/questions/questionsCompanies'
+
 const companyAnswers = {
   companyName: '',
   companyNameOpenField: 'Comment',
-  averageBusinessTripCost: 2,
-  averageBusinessTripCostOpenField: 'Open',
   numberOfEmployees: 2,
   numberOfEmployeesOpenField: 'Open',
+  topCosts: '1 2 3 4 5',
+  topCostsOpenField: 'Open',
   officeRentExpenses: 2,
   officeRentExpensesOpenField: '',
+  topEnergyActivities: '1 2 3 5 6',
+  topEnergyActivitiesOpenField: '',
+  energyCost: 2,
+  energyCostOpenField: '',
+  energySource: 'solar',
+  energySourceOpenField: '',
+  heatingCost: 2,
+  heatingCostOpenField: '',
+  heatingSource: 'electricity',
+  heatingSourceOpenField: '',
   otherUpkeepExpenses: 2,
-  otherUpkeepExpensesOpenField: ''
+  otherUpkeepExpensesOpenField: '',
+  totalCommutingSubsidies: 2,
+  totalCommutingSubsidiesOpenField: 'open',
+  averageFlightHours: 2,
+  averageFlightHoursOpenField: '',
+  averageCarHours: 2,
+  averageCarHoursOpenField: '',
+  shareOfRemoteWork: 25,
+  shareOfRemoteWorkOpenField: 'Open',
+  remoteWorkEase: 'difficult',
+  remoteWorkEaseOpenField: 'hard'
 }
 
 module.exports = companyAnswers
