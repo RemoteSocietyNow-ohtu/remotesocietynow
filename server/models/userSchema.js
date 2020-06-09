684d78cf12220355d8dff1fc66a081fd03cc892f
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-06-09 12:27:42 +0300

    lint

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -2,29 +2,29 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = mongoose.Schema({
-    username: {
-        type: String,
-        unique: true
-    },
-    name: String,
-    passwordHash: String,
-    answers: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'answers'
-        }
-    ],
+  username: {
+    type: String,
+    unique: true
+  },
+  name: String,
+  passwordHash: String,
+  answers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'answers'
+    }
+  ],
 })
 
 userSchema.plugin(uniqueValidator)
 
 userSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-        delete returnedObject.passwordHash
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
 })
 
 
