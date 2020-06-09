7d848be060f86ba105ba1a21d1cd15ab70caa88c
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-06-09 13:36:49 +0300

    edit userRouter

--- server/index.js
+++ server/index.js
@@ -5,8 +5,10 @@ app.use(express.json())
 
 const calculationRouter = require('./controllers/calculationRouter')
 const questionRouter = require('./controllers/questionRouter')
+const userRouter = require('./controllers/userRouter')
 
 app.use('/calculate/', calculationRouter)
 app.use('/questions/', questionRouter)
+app.use('/users/', userRouter)
 
 module.exports = app
