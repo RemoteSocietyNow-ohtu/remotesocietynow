446205cd198db73678dda9e186bc33b63778233d
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-06-10 14:07:37 +0300

    edit database now only handles database connections

--- server/database/database.js
+++ server/database/database.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
-const url = process.env.NODE_ENV === 'test' ? process.env.MONGODB_TEST_URI : process.env.MONGODB_URI
+const url = process.env.MONGODB_URI
 
 const connectToDatabase = () => {
   return mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-      console.log('connected to MongoDB')
+      console.log('connected to mongo')
     })
     .catch((error) => {
-      console.log('error connecting to MongoDB:', error.message)
+      console.log(error)
     })
 }
 
