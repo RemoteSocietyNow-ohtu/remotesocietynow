14d6be119202010e172c3eca10bfcb4249d5d3f3
Tambourin <olavi.hartonen@gmail.com>
2020-06-10 16:41:19 +0300

    Less obtrusive bottombar at results page

--- client/components/SubComponents/Calculator/Results.js
+++ client/components/SubComponents/Calculator/Results.js
@@ -9,7 +9,7 @@ import pollutionIcon from '../../../resources/pollution-icon.png'
 import co2SavedIcon from '../../../resources/co2-saved-icon.png'
 import NewsletterBox from '../Newsletter/NewsletterBox'
 
-import yourNewsletterIcon from '../../../resources/mail-icon-white.png'
+import atIcon from '../../../resources/at-white.png'
 import moneySavedIcon from '../../../resources/money-saved-icon.png'
 import moneySpentIcon from '../../../resources/money-spent-icon.png'
 
@@ -62,67 +62,73 @@ const Results = ({ results, answers, setAnwers, setResults, isCompany }) => {
   }
 
   return (
-    <div style={{ animation: 'none' }}>
-      <div className='Calculator-results-container'>
-        <div className='Calculator-results-left'>
-          <h1>Your Results</h1>
-          {
-            results.map(result =>
-              <div key={result.title}>
-                <div>
-                  <div className='Calculator-results-money-inline'>
-                    {!isCompany && result.bartype === 'greenbar' && <img className='Calculator-resultbar-icon' src={co2SavedIcon} alt='Pollution icon' />}
-                    {!isCompany && result.bartype === 'redbar' && <img className='Calculator-resultbar-icon' src={pollutionIcon} alt='Pollution icon' />}
-                    {isCompany && result.bartype === 'greenbar' && <img className='Calculator-resultbar-icon' src={moneySavedIcon} alt='Money saved icon' />}
-                    {isCompany && result.bartype === 'redbar' && <img className='Calculator-resultbar-icon' src={moneySpentIcon} alt='Money spent icon' />}
-                    <p className='Calculator-result-countup'><b></b><CountUp duration={.8} end={result.value ? result.value : 0} /> {result.unit}</p>
+    <>
+      <div style={{ animation: 'none' }}>
+        <div className='Calculator-results-container'>
+          <div className='Calculator-results-left'>
+            <h1>Your Results</h1>
+            {
+              results.map(result =>
+                <div key={result.title}>
+                  <div>
+                    <div className='Calculator-results-money-inline'>
+                      {!isCompany && result.bartype === 'greenbar' && <img className='Calculator-resultbar-icon' src={co2SavedIcon} alt='Pollution icon' />}
+                      {!isCompany && result.bartype === 'redbar' && <img className='Calculator-resultbar-icon' src={pollutionIcon} alt='Pollution icon' />}
+                      {isCompany && result.bartype === 'greenbar' && <img className='Calculator-resultbar-icon' src={moneySavedIcon} alt='Money saved icon' />}
+                      {isCompany && result.bartype === 'redbar' && <img className='Calculator-resultbar-icon' src={moneySpentIcon} alt='Money spent icon' />}
+                      <p className='Calculator-result-countup'><b></b><CountUp duration={.8} end={result.value ? result.value : 0} /> {result.unit}</p>
+                    </div>
+                    <p className='Calculator-results-result-title'>{result.title}</p>
                   </div>
-                  <p className='Calculator-results-result-title'>{result.title}</p>
                 </div>
-              </div>
-            )
-          }
-        </div>
-        <div className='Calculator-results-divider'></div>
-        <div className='Calculator-results-right'>
-          {
-            results.map(result =>
-              <div key={result.bartype} className='Calculator-results-resultbars'>
-                {!isCompany && result.bartype === 'greenbar' && <img className='Calculator-resultbar-icon' src={co2SavedIcon} alt='Pollution icon' />}
-                {!isCompany && result.bartype === 'redbar' && <img className='Calculator-resultbar-icon' src={pollutionIcon} alt='Pollution icon' />}
-                {isCompany && result.bartype === 'greenbar' && <img className='Calculator-resultbar-icon' src={moneySavedIcon} alt='Money saved icon' />}
-                {isCompany && result.bartype === 'redbar' && <img className='Calculator-resultbar-icon' src={moneySpentIcon} alt='Money spent icon' />}
-                <p></p>
-                <ResultBar width={80} percent={result.percent} type={result.bartype} />
-              </div>
-            )
-          }
-          <SliderField
-            handleValueChange={(event) => setSliderValue(event.target.value)}
-            handleRelease={handleRelease}
-            value={sliderValue}
-            minValue={0}
-            maxValue={isCompany ? 100 : 7}
-            unit={isCompany ? '%' : ''} // If this is results for a company use percents
-          />
-          {isCompany ?
-            <p className='Calculator-results-slidertext'>{language.headers.workDoneRemotelyPercent}</p> // If this is results for a company use percents
-            : <p className='Calculator-results-slidertext'>{language.headers.workDoneRemotelyDays}</p>
-          }
-        </div>        
-      </div>
-      
-      <div className='Calculator-results-bottom-navigation-bar'>
-        <p>We at RemoteSocietyNow are committed to work for better environment and to help you save money.</p>        
-        <div className='Calculator-results-bottom-navigation-bar-contacts'>
-          <p>Connect with us: </p>
-          <a className='Calculator-results-newsletter-button' onClick={() => setNewsletterOpen(true)}><img className='Main-bottom-bar-icon' src={yourNewsletterIcon} />{language.headers.subscribeToOurNewsletter}</a>
-          <a className='Calculator-results-newsletter-button'>Contact by Email</a>
+              )
+            }
+          </div>
+          <div className='Calculator-results-divider'></div>
+          <div className='Calculator-results-right'>
+            {
+              results.map(result =>
+                <div key={result.bartype} className='Calculator-results-resultbars'>
+                  {!isCompany && result.bartype === 'greenbar' && <img className='Calculator-resultbar-icon' src={co2SavedIcon} alt='Pollution icon' />}
+                  {!isCompany && result.bartype === 'redbar' && <img className='Calculator-resultbar-icon' src={pollutionIcon} alt='Pollution icon' />}
+                  {isCompany && result.bartype === 'greenbar' && <img className='Calculator-resultbar-icon' src={moneySavedIcon} alt='Money saved icon' />}
+                  {isCompany && result.bartype === 'redbar' && <img className='Calculator-resultbar-icon' src={moneySpentIcon} alt='Money spent icon' />}
+                  <p></p>
+                  <ResultBar width={80} percent={result.percent} type={result.bartype} />
+                </div>
+              )
+            }
+            <SliderField
+              handleValueChange={(event) => setSliderValue(event.target.value)}
+              handleRelease={handleRelease}
+              value={sliderValue}
+              minValue={0}
+              maxValue={isCompany ? 100 : 7}
+              unit={isCompany ? '%' : ''} // If this is results for a company use percents
+            />
+            {isCompany ?
+              <p className='Calculator-results-slidertext'>{language.headers.workDoneRemotelyPercent}</p> // If this is results for a company use percents
+              : <p className='Calculator-results-slidertext'>{language.headers.workDoneRemotelyDays}</p>
+            }
+          </div>        
         </div>
         
-      </div>      
+        <div className='Calculator-results-bottom-navigation-bar'>
+          
+          <p>We at RemoteSocietyNow are committed to work for better environment and to help you save money.</p>        
+          <div className='Calculator-results-bottom-navigation-bar-contacts'>
+            <img className='Calculator-results-bottom-navigation-bar-icon' src={atIcon} />
+            <p>Connect with us: </p>
+            <a className='Calculator-results-newsletter-button' onClick={() => setNewsletterOpen(true)}>{language.headers.subscribeToOurNewsletter}</a>
+            <p>|</p>
+            <a className='Calculator-results-newsletter-button'> Contact by Email</a>
+          </div>
+          
+        </div>      
+        
+      </div>
       <NewsletterBox open={newsletterOpen} setOpen={setNewsletterOpen}/>
-    </div>
+    </>
   )
 }
 
