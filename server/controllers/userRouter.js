5cbb90ed0830f83aeb89a4a70001086ea7a6189c
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-06-10 14:48:38 +0300

    add jwt-login boilerplate

--- server/controllers/userRouter.js
+++ server/controllers/userRouter.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcrypt')
 const userRouter = require('express').Router()
 const User = require('../models/userSchema')
+const jwt = require('jsonwebtoken')
+
+
 
 userRouter.post('/', async (request, response) => {
   const body = request.body
@@ -17,6 +20,32 @@ userRouter.post('/', async (request, response) => {
   console.log('asd')
 
   response.json(user)
+})
+
+userRouter.post('/login/', async (req, res) => {
+  const body = req.body
+
+  const user = await User.findOne({ username: body.username })
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(body.password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  res
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
 })
 
 module.exports = userRouter
