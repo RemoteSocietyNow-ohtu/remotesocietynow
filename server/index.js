6e675fdba21940e7960083fc736934833c9747aa
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-06-11 16:46:00 +0300

    add index now uses filerouter

--- server/index.js
+++ server/index.js
@@ -8,11 +8,13 @@ const calculationRouter = require('./controllers/calculationRouter')
 const questionRouter = require('./controllers/questionRouter')
 const newsletterRouter = require('./controllers/newsletterRouter')
 const userRouter = require('./controllers/userRouter')
+const fileRouter = require('./controllers/fileRouter')
 
 app.use('/newsletter/', newsletterRouter)
 app.use('/calculate/', calculationRouter)
 app.use('/questions/', questionRouter)
 app.use('/users/', userRouter)
+app.use('/files/', fileRouter)
 
 
 module.exports = app
