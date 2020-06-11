de7cd8037d1f4915d8fcd3c87fac12d1841ffabc
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-06-11 17:06:53 +0300

    feature toggle for csv downloading

--- server/controllers/fileRouter.js
+++ server/controllers/fileRouter.js
@@ -4,16 +4,20 @@ const Company = require('../models/companySchema')
 const questions = require('../services/questions/questionsCompanies')
 
 fileRouter.get('/csv/', async (req, res) => {
-  console.log('hei')
-  const csv = await db.dataToCSV(questions,Company)
 
-  res.setHeader('Content-disposition', 'attachment; filename=data.csv');
-  res.setHeader('Content-type', 'text/csv');
+    if (process.env.NODE_ENV === 'development') {
 
-  res.write(csv, function (err) {
-    res.end();
-  })
+        const csv = await db.dataToCSV(questions, Company)
 
+        res.setHeader('Content-disposition', 'attachment; filename=data.csv');
+        res.setHeader('Content-type', 'text/csv');
+
+        res.write(csv, function (err) {
+            res.end();
+        })
+    }else{
+        res.send('work in progress')
+    }
 })
 
 module.exports = fileRouter
