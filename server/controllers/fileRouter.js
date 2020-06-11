68c14d5ff5ab09311205b9f8c07fbd0a97a1a96c
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-06-11 17:08:06 +0300

    fix lint

--- server/controllers/fileRouter.js
+++ server/controllers/fileRouter.js
@@ -5,19 +5,19 @@ const questions = require('../services/questions/questionsCompanies')
 
 fileRouter.get('/csv/', async (req, res) => {
 
-    if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development') {
 
-        const csv = await db.dataToCSV(questions, Company)
+    const csv = await db.dataToCSV(questions, Company)
 
-        res.setHeader('Content-disposition', 'attachment; filename=data.csv');
-        res.setHeader('Content-type', 'text/csv');
+    res.setHeader('Content-disposition', 'attachment; filename=data.csv')
+    res.setHeader('Content-type', 'text/csv')
 
-        res.write(csv, function (err) {
-            res.end();
-        })
-    }else{
-        res.send('work in progress')
-    }
+    res.write(csv, function () {
+      res.end()
+    })
+  }else{
+    res.send('work in progress')
+  }
 })
 
 module.exports = fileRouter
