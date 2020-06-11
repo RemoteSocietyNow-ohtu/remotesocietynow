1b57d166e65264caa0b8208c6fca7e9f47d6c816
Tambourin <olavi.hartonen@gmail.com>
2020-06-11 23:46:16 +0300

    mailService now uses Ethereal test mail account for test and dev

--- server/controllers/newsletterRouter.js
+++ server/controllers/newsletterRouter.js
@@ -1,13 +1,9 @@
 const newsletterRouter = require('express').Router()
-//const ProtonMail = require('protonmail-api')
 const newsletterWelcomeMessage = require('../services/newsletterTemplates/newsletterWelcomeMessage')
 const mailService = require('../services/mailService/mailService')
 
 newsletterRouter.post('/', async (req, res) => {
   const address = req.body.address
-  if (process.env.NODE_ENV === 'test') {
-    return res.sendStatus(200)
-  }
 
   try {
     await mailService.sendMail(address, newsletterWelcomeMessage.subject, newsletterWelcomeMessage.body)
--- server/services/mailService/mailService.js
+++ server/services/mailService/mailService.js
@@ -1,31 +1,61 @@
 const nodemailer = require('nodemailer')
+const util = require('util')
 
-const sendMail = async (address, subject, body) => {
-  let transporter = await nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // use SSL
-    auth: {
-      user: process.env.MAIL_USERNAME,
-      pass: process.env.MAIL_PASSWORD
+let transporter
+ 
+const createTransport = async () => {
+  let mailConfig
+  if (process.env.NODE_ENV === 'production' ) {
+    mailConfig = {
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // use SSL
+      auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD
+      }
     }
-  })
+  } else {  
+    const createTestAccount = util.promisify(nodemailer.createTestAccount)
+    const testAccout = await createTestAccount()
+    mailConfig = {
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: testAccout.user,
+        pass: testAccout.pass
+      }
+    }
+  }
+  transporter = nodemailer.createTransport(mailConfig)
+}
 
-  var mailOptions = {
+const sendToAdmin = (address) => {
+  const adminMail = {
+    from: process.env.MAIL_USERNAME,
+    to: process.env.MAIL_USERNAME,
+    subject: 'New RemoteSocietyNow-newsletter Subscription',
+    html: `${address} wants to join RemoteSocietyNow-newsletter`
+  }
+  return transporter.sendMail(adminMail)
+}
+
+const sendToSubscriber = (address, subject, body) => {
+  const mailOptions = {
     from: process.env.MAIL_USERNAME,
     to: address,
     subject: subject,
     html: body
-  }
-  
-  await transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error)
-    } else {
-      console.log('Email sent: ' + info.response)
-    }
-  })  
-  
+  }  
+  return transporter.sendMail(mailOptions)
+}
+
+const sendMail = async (address, subject, body) => { 
+  await createTransport() 
+  const mailToAdmin = await sendToAdmin(address)
+  console.log('test message to admin: ', nodemailer.getTestMessageUrl(mailToAdmin))
+  const mailToSubscriber = await sendToSubscriber(address, subject, body)
+  console.log('test message to subscriber: ', nodemailer.getTestMessageUrl(mailToSubscriber))      
 }
 
 module.exports = { sendMail }
