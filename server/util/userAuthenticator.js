d1142a40028bbd41bd9cd8d593a7b60c6a05cfd0
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-06-12 13:51:40 +0300

    add user authenticator

---
+++ server/util/userAuthenticator.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+
+
+const getTokenFrom = req => {
+
+    const authorization = req.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
+const isAdmin = (req) => {
+
+    const token = getTokenFrom(req)
+
+    if (token !== null) {
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+        console.log(decodedToken)
+        if (decodedToken.role === 'ADMIN')
+            return true
+    }
+
+    return false
+}
+
+module.exports = {
+    isAdmin
+}
