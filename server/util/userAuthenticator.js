44cc4c8e7c52c0780e54da667b5bb70f42c4f0a6
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-06-12 13:59:47 +0300

    fix lint

--- server/util/userAuthenticator.js
+++ server/util/userAuthenticator.js
@@ -3,27 +3,27 @@ const jwt = require('jsonwebtoken')
 
 const getTokenFrom = req => {
 
-    const authorization = req.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer')) {
-        return authorization.substring(7)
-    }
-    return null
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    return authorization.substring(7)
+  }
+  return null
 }
 
 const isAdmin = (req) => {
 
-    const token = getTokenFrom(req)
+  const token = getTokenFrom(req)
 
-    if (token !== null) {
-        const decodedToken = jwt.verify(token, process.env.SECRET)
-        console.log(decodedToken)
-        if (decodedToken.role === 'ADMIN')
-            return true
-    }
+  if (token !== null) {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    console.log(decodedToken)
+    if (decodedToken.role === 'ADMIN')
+      return true
+  }
 
-    return false
+  return false
 }
 
 module.exports = {
-    isAdmin
+  isAdmin
 }
