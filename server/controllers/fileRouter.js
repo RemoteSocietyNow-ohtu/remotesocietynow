d8f950e56b4fc9394d5a3be5768d886b6e0cd212
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-06-12 13:54:17 +0300

    edit csv is now only downloadable by admmin

--- server/controllers/fileRouter.js
+++ server/controllers/fileRouter.js
@@ -2,20 +2,24 @@ const fileRouter = require('express').Router()
 const db = require('../database/database')
 const Company = require('../models/companySchema')
 const questions = require('../services/questions/questionsCompanies')
+const authenticator = require('../util/userAuthenticator')
 
 fileRouter.get('/csv/', async (req, res) => {
-
   if (process.env.NODE_ENV === 'development') {
 
-    const csv = await db.dataToCSV(questions, Company)
+    if (authenticator.isAdmin(req)) {
+      const csv = await db.dataToCSV(questions, Company)
 
-    res.setHeader('Content-disposition', 'attachment; filename=data.csv')
-    res.setHeader('Content-type', 'text/csv')
+      res.setHeader('Content-disposition', 'attachment; filename=data.csv')
+      res.setHeader('Content-type', 'text/csv')
 
-    res.write(csv, function () {
-      res.end()
-    })
-  }else{
+      res.write(csv, function () {
+        res.end()
+      })
+    } else {
+      res.send('not authenticated')
+    }
+  } else {
     res.send('work in progress')
   }
 })
