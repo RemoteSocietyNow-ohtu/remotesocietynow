459140f5f42e585fb0ee868f2f525b0181839153
universami <sami.toiskallio@protonmail.com>
2020-06-15 13:35:10 +0300

    add logout functionality

--- client/components/Navigation/Navbar.js
+++ client/components/Navigation/Navbar.js
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react'
 import logo from '../../resources/logo512.png'
 import LanguageContext from '../../Contexts/LanguageContext'
+import authenticationService from '../../services/authenticationService'
 
 const Navbar = ({ body, setBody, Cookies }) => {
 
   const language = useContext(LanguageContext)
 
-
+  const handleLogout = () => {
+    authenticationService.logout(Cookies)    
+  }
 
   return (
     <div>
@@ -17,6 +20,12 @@ const Navbar = ({ body, setBody, Cookies }) => {
           {!Cookies.get('token') && !Cookies.get('adminToken') && <div className='Navbar-authentication-div'>
             {body !== 'login' && <button className='Navbar-login-button' onClick={() => setBody('login')}>Login</button>}
             {body !== 'signUp' && <p className='Navbar-sign-in' onClick={() => setBody('signUp')}>{language.buttons.signUp}</p>}
+          </div>}
+          {Cookies.get('token') && <div className='Navbar-authentication-div'>
+            <button className='Navbar-logout-button' onClick={() => authenticationService.logout(Cookies)}>Logout</button>
+          </div>}
+          {Cookies.get('adminToken') && <div className='Navbar-authentication-div'>
+            <button className='Navbar-logout-button' onClick={() => handleLogout()}>Logout</button>
           </div>}
         </nav>
       </header>
