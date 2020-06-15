4ea56bcd87d515bd60ce74e78eefe5a2839a3b53
universami <sami.toiskallio@protonmail.com>
2020-06-15 16:01:42 +0300

    revoke http to https changes

--- server/index.js
+++ server/index.js
@@ -4,10 +4,6 @@ const app = express()
 
 app.use(express.json())
 
-app.get('*', function(req, res) {
-  res.redirect('https://' + req.headers.host + req.url)
-})
-
 const calculationRouter = require('./controllers/calculationRouter')
 const questionRouter = require('./controllers/questionRouter')
 const newsletterRouter = require('./controllers/newsletterRouter')
--- server/tests/questionIntegration.test.js
+++ server/tests/questionIntegration.test.js
@@ -1,27 +1,21 @@
 require('@babel/polyfill')
 const supertest = require('supertest')
 const app = require('../index')
-// const questionsPeople = require('../services/questions/questionsPeople')
-// const questionsCompanies = require('../services/questions/questionsCompanies')
+const questionsPeople = require('../services/questions/questionsPeople')
+const questionsCompanies = require('../services/questions/questionsCompanies')
 
-test('Dummy test', async () => {
-  await supertest(app)
+test('Can get questions for companies', async () => {
+  const response = await supertest(app)
     .get('/questions/companies/')
-  expect(1).toBe(1)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  expect(response.body.length).toBe(questionsCompanies.length)
 })
 
-// test('Can get questions for companies', async () => {
-//   const response = await supertest(app)
-//     .get('/questions/companies/')
-//     .expect(302)
-//     .expect('Content-Type', /application\/json/)
-//   expect(response.body.length).toBe(questionsCompanies.length)
-// })
-
-// test('Can get questions for employees', async () => {
-//   const response = await supertest(app)
-//     .get('/questions/people/')
-//     .expect(302)
-//     .expect('Content-Type', /application\/json/)
-//   expect(response.body.length).toBe(questionsPeople.length)  
-// })
+test('Can get questions for employees', async () => {
+  const response = await supertest(app)
+    .get('/questions/people/')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  expect(response.body.length).toBe(questionsPeople.length)  
+})
