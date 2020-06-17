bb271e7c7ab82d5ffdf3ea1abdd274776ae5dac8
Sami <samialeksi.tuomela@gmail.com>
2020-06-17 15:30:12 +0300

    added partial about us

--- client/components/Views/AboutUs.js
+++ client/components/Views/AboutUs.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import Projectinfo from '../SubComponents/Projectinfo'
-import Mathinfo from '../SubComponents/Mathinfo'
 import Video from '../SubComponents/Video'
 
-const AboutUs = () => {
+const AboutUs = ({ setBody }) => {
   return (
     <div>
       <h1 className='AboutUs-header'>About Us</h1>
       <Video style={{'maxWidth': '80%'}} />
       <Projectinfo />
-      <Mathinfo />
+      <button className='AboutUs-calculatoChoice-button' onClick={() => setBody('calculatorChoice')}>Test your reductions now!</button>
     </div>
   )
 }
