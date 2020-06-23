4b259675d2ffca21ca8c24219894f62d0848d256
Tambourin <olavi.hartonen@gmail.com>
2020-06-23 08:25:51 +0300

    fix adminSettingsService

--- server/services/adminSettingsService.js
+++ server/services/adminSettingsService.js
@@ -1,7 +1,7 @@
-const AdminSettings = require('../models/AdminSettingsSchema')
+const adminSettings = require('../models/adminSettingsSchema')
 
 const getSaveToDatabase = async () => {
-  const settings = await AdminSettings.findOne({ })
+  const settings = await adminSettings.findOne({ })
   return settings.saveDataToDatabase
 }
 
