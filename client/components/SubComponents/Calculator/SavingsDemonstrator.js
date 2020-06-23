11e90890efeda83ba2e126fde0517e4899836d5e
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-06-23 15:37:40 +0300

    fix lint

--- client/components/SubComponents/Calculator/SavingsDemonstrator.js
+++ client/components/SubComponents/Calculator/SavingsDemonstrator.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const SavingsDemonstrator = ({results}) => {
-    const result = results.find(result => result.bartype === 'greenbar')
-    return (
+  const result = results.find(result => result.bartype === 'greenbar')
+  return (
     <div className='Calculator-results-savings-container'>
-        <p>This is equal to about {result.value/0.155} kilometers driven in a car.</p>
+      <p>This is equal to about {result.value/0.155} kilometers driven in a car.</p>
     </div>
-    )
+  )
 }
 
 export default SavingsDemonstrator
