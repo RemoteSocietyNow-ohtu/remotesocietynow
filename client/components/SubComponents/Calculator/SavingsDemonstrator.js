526e481ebf0658802df7288dfcb6b5ae5d8ea024
Roeoeri <tomi.haapakoski@helsinki.fi>
2020-06-24 10:17:26 +0300

    fix lint

--- client/components/SubComponents/Calculator/SavingsDemonstrator.js
+++ client/components/SubComponents/Calculator/SavingsDemonstrator.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const SavingsDemonstrator = ({results, answers}) => {
-    const result = results.find(result => result.bartype === 'greenbar')
-    return (
+  const result = results.find(result => result.bartype === 'greenbar')
+  return (
     <div onClick={(event) => event.stopPropagation()} className='Calculator-results-savings-container'>
-        <p>By doing remote work on <b>{answers.numberOfRemoteworkDays}</b> days per week, you save <b>{result.value}</b> kg of emissions. This is equal to about: </p>
-        <p><b>{Math.floor(result.value/0.155)}</b> kilometers driven in a car</p>
-        <p><b>{Math.round(result.value/705 * 10)/10}</b> flights over the Atlantic Ocean</p>
+      <p>By doing remote work on <b>{answers.numberOfRemoteworkDays}</b> days per week, you save <b>{result.value}</b> kg of emissions. This is equal to about: </p>
+      <p><b>{Math.floor(result.value/0.155)}</b> kilometers driven in a car</p>
+      <p><b>{Math.round(result.value/705 * 10)/10}</b> flights over the Atlantic Ocean</p>
     </div>
   )
 }
--- client/components/Views/DeletionConfirmation.js
+++ client/components/Views/DeletionConfirmation.js
@@ -2,13 +2,15 @@ import React, { useContext } from 'react'
 import LanguageContext from '../../Contexts/LanguageContext'
 import arrowLeft from '../../resources/arrow-left.png'
 import deleteIcon from '../../resources/delete-icon.png'
+import fileservise from '../../services/fileService'
 
-const DeletionConfirmation = ({ setBody }) => {
+const DeletionConfirmation = ({Cookies, setBody }) => {
 
   const language = useContext(LanguageContext)
+  let token = Cookies.get('token')
 
   const deleteUserData = () => {
-    // todo
+    fileservise.deleteUser(token)
   }
 
   return (
