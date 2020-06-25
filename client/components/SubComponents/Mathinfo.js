7c59b7fe2c42331565338e5cb374de5afc425343
Tambourin <olavi.hartonen@gmail.com>
2020-06-25 12:59:11 +0300

    Fixed mathInfo alignment

--- client/components/SubComponents/Mathinfo.js
+++ client/components/SubComponents/Mathinfo.js
@@ -7,7 +7,7 @@ const Mathinfo = ({onClick}) => {
   return (
     <div id='mathinfo' className='Mathinfo-container'>
       <h2>{language.headers.aboutTheMath}</h2>
-      <p>{language.mathInfo}</p>
+      <p className='Mathinfo-paragraph'>{language.mathInfo}</p>
       <a className='Mathinfo-x-button' onClick={onClick}>
         <img style={{height: '1.5rem'}} src={cross} />
       </a>
